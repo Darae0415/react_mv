85bd7c2fa059b194eec5efd31aed9b2ff265ee4c
Darae0415 <ekfogksmf@naver.com>
2024-07-29 12:38:20 +0900

    step4 영화목록읽어보기

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './step3/App';
+import App from './step4/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
